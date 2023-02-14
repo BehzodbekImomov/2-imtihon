99c76ed25d705af971e3644f8c000578f7316c52
mr <behzodbeki65@gmail.com>
2023-02-15 02:47:49 +0300

    htmlni bitirdim

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const burger_btn=document.querySelector("#burger_btn")
+const close_btn=document.querySelector(".close_btn img")
 const animationNavigation=document.querySelector(".animatedNavigation")
 
 
@@ -6,12 +7,19 @@ const animationNavigation=document.querySelector(".animatedNavigation")
 
 
 function showNav(){
+    if(animationNavigation.classList.contains("active")){
+
+        animationNavigation.classList.remove("active") 
+    }else{
+        animationNavigation.classList.add("active") 
+    }
 console.dir(animationNavigation)
 
-animationNavigation.classList.add("active") 
+
 }
 
 burger_btn.addEventListener("click",showNav)
+close_btn.addEventListener("click",showNav)
 
 
 
